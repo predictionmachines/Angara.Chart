9c3b090456e7add6bc1cfa00ce5df7bbfb60a156
ElenaPochernina <elenap@itis.cs.msu.ru>
2017-04-06 12:21:04 +0300

    Bump version to 0.2.4

--- src/Angara.Chart/AssemblyInfo.fs
+++ src/Angara.Chart/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Angara.Chart")>]
 [<assembly: AssemblyProductAttribute("Angara.Chart")>]
 [<assembly: AssemblyDescriptionAttribute("Data visualization library for F#.")>]
-[<assembly: AssemblyVersionAttribute("0.2.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.2")>]
+[<assembly: AssemblyVersionAttribute("0.2.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.2"
-    let [<Literal>] InformationalVersion = "0.2.2"
+    let [<Literal>] Version = "0.2.4"
+    let [<Literal>] InformationalVersion = "0.2.4"
